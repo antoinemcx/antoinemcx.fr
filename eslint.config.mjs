6f1a6b5de16d89c08fd84301f9720f1cc2be93bd
antoinemcx <antoniodu1333@gmail.com>
2025-08-05 22:10:42 +0200

    🔧 ESLint custom configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,17 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import antfu from "@antfu/eslint-config";
 
 export default withNuxt(
-  // Your custom configs here
-)
+  antfu({
+    rules: {
+      "ts/no-redeclare": "off",
+      "eslint-comments/no-unlimited-disable": "off",
+      "style/brace-style": ["error", "1tbs"],
+    },
+    stylistic: {
+      semi: true,
+      quotes: "double",
+    },
+  })
+);
