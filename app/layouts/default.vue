5a010b1275c19419c252e1775ea072f68e3da165
antoinemcx <antoniodu1333@gmail.com>
2025-08-10 15:04:56 +0200

    💄 Navbar shadow when page scrolled

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,28 @@
+<script setup lang="ts">
+const isScrolled = ref(false);
+
+function handleScroll() {
+  isScrolled.value = window.scrollY > 20;
+}
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
+
 <template>
   <div class="min-h-screen flex flex-col">
     <!-- Navigation bar fixed at the top -->
-    <div class="fixed top-0 left-0 right-0 z-50 bg-[var(--ui-bg)]">
+    <div
+      class="fixed top-0 left-0 right-0 z-50 bg-[var(--ui-bg)] transition-shadow duration-300"
+      :class="{
+        'shadow-sm dark:shadow-md shadow-neutral-200/75 dark:shadow-neutral-800/25': isScrolled,
+      }"
+    >
       <div class="max-w-5xl mx-auto px-4 md:px-6">
         <Navbar />
       </div>
