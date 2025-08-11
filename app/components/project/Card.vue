085e7c41580db50270ebb136f99b279de6b35727
antoinemcx <antoniodu1333@gmail.com>
2025-08-11 23:53:43 +0200

    🐛 Project cards line must have the same height

--- app/components/project/Card.vue
+++ app/components/project/Card.vue
@@ -32,9 +32,11 @@ const projectPath = props.project.path.replace(`/${locale.value}/`, "/");
 </script>
 
 <template>
-  <!-- TODO same height cards -->
   <a :href="projectPath" class="active:translate-y-0.5 transition-transform duration-200">
-    <UCard class="group cursor-pointer">
+    <UCard
+      class="group cursor-pointer hover:ring-accented transition-all duration-500 h-full"
+      :ui="{ body: 'h-full flex flex-col' }"
+    >
       <!-- Header -->
       <div class="flex gap-2 items-center text-lg font-bold mb-1">
         <NuxtImg :src="project.logo" :alt="project.name" class="size-7 rounded-full" />
@@ -46,65 +48,69 @@ const projectPath = props.project.path.replace(`/${locale.value}/`, "/");
         {{ project.description }}
       </p>
 
-      <div
-        v-if="(project.status && project.status !== ProjectStatus.ACTIVE)
-          || project.isCollaborator"
-        class="flex flex-col gap-2 mt-4 mb-2"
-      >
-        <!-- TODO GH stars -->
-
-        <!-- Status and collaborator badge -->
+      <div class="flex flex-col gap-2 mt-auto">
         <div
           v-if="(project.status && project.status !== ProjectStatus.ACTIVE)
             || project.isCollaborator"
-          class="flex gap-1"
+          class="flex flex-col gap-2 mt-4"
         >
-          <UBadge
-            v-if="project.isCollaborator"
-            color="warning"
-            variant="soft"
-            class="rounded-full"
-          >
-            {{ t("projects.isCollaborator") }}
-          </UBadge>
+          <!-- TODO GH stars -->
 
-          <UBadge
-            v-if="project.status && project.status !== ProjectStatus.ACTIVE"
-            :color="status.color"
-            variant="soft"
-            class="rounded-full"
+          <!-- Status and collaborator badge -->
+          <div
+            v-if="(project.status && project.status !== ProjectStatus.ACTIVE)
+              || project.isCollaborator"
+            class="flex gap-1"
           >
-            {{ status.label }}
-          </UBadge>
+            <UBadge
+              v-if="project.isCollaborator"
+              color="warning"
+              variant="soft"
+              class="rounded-full"
+            >
+              {{ t("projects.isCollaborator") }}
+            </UBadge>
+
+            <UBadge
+              v-if="project.status && project.status !== ProjectStatus.ACTIVE"
+              :color="status.color"
+              variant="soft"
+              class="rounded-full"
+            >
+              {{ status.label }}
+            </UBadge>
+          </div>
+
+          <!-- TODO technos -->
+
+          <!-- Tags for large cards -->
+          <div
+            v-if="isLargeCard && project.tags && project.tags.length > 0"
+            class="flex flex-wrap gap-1"
+          >
+            <UBadge
+              v-for="tag in project.tags"
+              :key="tag"
+              color="neutral"
+              variant="soft"
+              class="rounded-full"
+            >
+              {{ tag }}
+            </UBadge>
+          </div>
         </div>
 
-        <!-- Tags for large cards -->
-        <div
-          v-if="isLargeCard && project.tags && project.tags.length > 0"
-          class="flex flex-wrap gap-1 mb-2"
-        >
-          <UBadge
-            v-for="tag in project.tags"
-            :key="tag"
-            color="neutral"
-            variant="soft"
-            class="rounded-full"
-          >
-            {{ tag }}
-          </UBadge>
+        <!-- View more button -->
+        <div class="w-full flex justify-end items-center">
+          <span class="opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+            {{ t("home.featuredProjects.readMore") }}
+          </span>
+          <UIcon
+            name="i-lucide-move-right"
+            class="size-5 text-muted transition-all duration-500
+          group-hover:translate-x-1 group-hover:text-default"
+          />
         </div>
-      </div>
-
-      <!-- View more button -->
-      <div class="w-full flex justify-end items-center">
-        <span class="opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-          {{ t("home.featuredProjects.readMore") }}
-        </span>
-        <UIcon
-          name="i-lucide-move-right"
-          class="size-5 text-muted transition-transform duration-500
-        group-hover:translate-x-1 group-hover:text-default"
-        />
       </div>
     </UCard>
   </a>
