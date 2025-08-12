e5b65a52048ea6a446c484f83caef702118b0bae
antoinemcx <antoniodu1333@gmail.com>
2025-08-12 21:23:47 +0200

    ✨ Projects display limit

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 const { t } = useI18n();
 
+const initialProjectsCount = 8;
+const displayAllProjects = ref(false);
+
 /* Fetch all projects data */
 const { data: projects, status } = await useProjectsContent();
 </script>
@@ -10,25 +13,53 @@ const { data: projects, status } = await useProjectsContent();
     {{ t("projects.title") }}
   </h1>
   <p class="text-muted mb-5">
-    {{ t("home.featuredProjects.description") }}
+    {{ t("projects.description") }}
   </p>
 
   <div v-if="status === 'pending'" class="grid grid-cols-1 md:grid-cols-2 gap-3">
-    <UCard v-for="n in 6" :key="n">
-      <USkeleton class="h-36" />
+    <UCard v-for="n in initialProjectsCount" :key="n">
+      <USkeleton class="h-40" />
     </UCard>
   </div>
-
   <div v-else-if="!projects || projects.length === 0" class="text-muted">
     {{ t("projects.empty") }}
   </div>
 
-  <div v-else class="grid grid-cols-1 md:grid-cols-2 gap-3">
-    <ProjectCard
-      v-for="project in projects"
-      :key="project.id"
-      :project="project"
-      is-large-card
-    />
+  <div v-else>
+    <!-- Initial list of initialProjectsCount projects -->
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
+      <ProjectCard
+        v-for="project in projects.slice(0, initialProjectsCount)"
+        :key="project.id"
+        :project="project"
+        is-large-card
+      />
+    </div>
+
+    <!-- Other projects with a "view more" button -->
+    <div v-if="displayAllProjects" class="grid grid-cols-1 md:grid-cols-2 gap-3 mt-3">
+      <ProjectCard
+        v-for="project in projects.slice(initialProjectsCount)"
+        :key="project.id"
+        :project="project"
+        is-large-card
+      />
+    </div>
+
+    <div class="flex justify-center mt-4">
+      <UButton
+        v-if="projects && projects.length > initialProjectsCount"
+        color="neutral"
+        variant="outline"
+        :trailing-icon="displayAllProjects ? 'lucide:arrow-up' : 'lucide:arrow-down'"
+        class="group w-fit active:translate-y-0.5 transition-transform duration-200"
+        :ui="{
+          trailingIcon: 'group-hover:scale-105 transition-transform duration-200',
+        }"
+        @click="displayAllProjects = !displayAllProjects"
+      >
+        {{ displayAllProjects ? t("projects.viewLess") : t("projects.viewMore") }}
+      </UButton>
+    </div>
   </div>
 </template>
