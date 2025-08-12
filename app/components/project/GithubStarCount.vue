5dc55eb2b1a786c491ec0a45185be1c5a610091d
antoinemcx <antoniodu1333@gmail.com>
2025-08-12 09:58:07 +0200

    🐛 Skeleton loader not effective

--- app/components/project/GithubStarCount.vue
+++ app/components/project/GithubStarCount.vue
@@ -10,7 +10,7 @@ if (repoUrl.length > 2) {
   repoUrl = repoUrl.slice(0, 2); // remove branches or tags data
 }
 
-const { data } = await useLazyFetch<{
+const { data, status } = await useLazyFetch<{
   stargazers_count: number; // other fields are not useful here
 }>(`https://api.github.com/repos/${repoUrl.join("/")}`);
 
@@ -18,12 +18,15 @@ const githubStarCount = data.value?.stargazers_count;
 </script>
 
 <template>
-  <div v-if="githubStarCount === undefined" class="flex gap-1 items-center">
+  <div v-if="status === 'pending'" class="flex gap-1 items-center">
     <UIcon name="i-lucide-star" class="size-3.5" />
     <USkeleton class="h-5 w-8" />
   </div>
 
-  <div v-else class="flex gap-1 items-center">
+  <div
+    v-else-if="status === 'success' && githubStarCount !== undefined"
+    class="flex gap-1 items-center"
+  >
     <UIcon name="i-lucide-star" class="size-3.5" />
     <span>{{ githubStarCount }}</span>
   </div>
