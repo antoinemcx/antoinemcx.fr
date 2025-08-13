9aeee389cca816685fe5ba186110bb82834802d1
antoinemcx <antoniodu1333@gmail.com>
2025-08-13 10:41:05 +0200

    🐛 Caching strategy to avoid refetching when switching page or applying filters

--- app/components/project/GithubMetrics.vue
+++ app/components/project/GithubMetrics.vue
@@ -3,6 +3,7 @@ const props = defineProps<{
   githubRepositoryUrl: string;
 }>();
 
+const nuxtApp = useNuxtApp();
 const baseUrl = "https://github.com/";
 
 let repoUrl = props.githubRepositoryUrl.replace(baseUrl, "").split("/");
@@ -13,18 +14,43 @@ if (repoUrl.length > 2) {
 const { data, status } = await useLazyFetch<{
   stargazers_count: number;
   forks_count: number; // other fields are not useful here
-}>(`https://api.github.com/repos/${repoUrl.join("/")}`);
+}>(`https://api.github.com/repos/${repoUrl.join("/")}`, {
+  transform(input) {
+    return { ...input, fetchedAt: new Date() };
+  },
+  getCachedData(key) { // check cache, refetch if nullish value
+    let data = nuxtApp.payload.data[key] || nuxtApp.static.data[key];
 
-const githubStarCount = data.value?.stargazers_count;
-const githubForkCount = data.value?.forks_count;
+    /* Check for data expiration */
+    if (data) {
+      const expirationDate = new Date(data.fetchedAt);
+      expirationDate.setTime(expirationDate.getTime() + 1000 * 60 * 5); // 5 min
+      if (expirationDate.getTime() < Date.now()) {
+        data = null; // data expired, null to force refetch
+      }
+    }
+
+    return data;
+  },
+});
+
+const githubStarCount = computed(() => data.value?.stargazers_count);
+const githubForkCount = computed(() => data.value?.forks_count);
 </script>
 
 <template>
-  <div v-if="status === 'pending'" class="flex gap-1 items-center">
-    <UIcon name="i-lucide-star" class="size-3.5" />
-    <USkeleton class="h-5 w-6" />
+  <div v-if="status === 'pending'" class="flex gap-2 items-center">
+    <div class="flex gap-1 items-center">
+      <UIcon name="lucide:star" class="size-3.5" />
+      <USkeleton class="h-5 w-6" />
+    </div>
+    <div class="flex gap-1 items-center">
+      <UIcon name="lucide:git-fork" class="size-3.5" />
+      <USkeleton class="h-5 w-6" />
+    </div>
   </div>
 
+  <!-- Show metrics if no error -->
   <div
     v-else-if="status === 'success' && githubStarCount !== undefined"
     class="flex gap-1 items-center"
