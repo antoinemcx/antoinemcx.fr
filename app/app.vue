ebaef4af5f1928ffe5c60dfc1e4e77e8a95a6b02
antoinemcx <antoniodu1333@gmail.com>
2026-05-09 01:19:10 +0200

    🔍️ OG Image

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const { locale } = useI18n();
+const { origin } = useRequestURL();
 useHead({
   htmlAttrs: {
     lang: locale,
@@ -10,7 +11,8 @@ useSeoMeta({
   ogTitle: "Antoine M.",
   description: "I'm Antoine, a French software engineer. Learn more about me, my work and my projects on this website.",
   ogDescription: "I'm Antoine, a French software engineer. Learn more about me, my work and my projects on this website.",
-  themeColor: "#2A7FFF",
+  themeColor: "#45556c",
+  ogImage: `${origin}/og-image.png`,
 });
 </script>
 
